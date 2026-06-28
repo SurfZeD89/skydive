3aad823a80d9d9740e05997edbeed1a64e3a2397
SurfZeD89 <110341088+SurfZeD89@users.noreply.github.com>
2026-06-28 15:32:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-self.addEventListener('fetch',e=>{});
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    // לא משתמשים בקאש כרגע
+});
